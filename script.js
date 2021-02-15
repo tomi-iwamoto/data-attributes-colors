c5b76dbcd88ad580d566596dc6d530250f9991b4
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-15 00:09:05 -0500

    Created nodelist from box classes and a new array from the nodelist. Then used forEach method to add an event listener to each element with class of box. Used dataset as the inner text of the target elements. Also added a clear button on the html file.

---
+++ script.js
@@ -0,0 +1,24 @@
+//When I click on the boxes, each box will show a text of the name of color of box
+
+
+//Getting all elements with the class of box
+let box = document.querySelectorAll('.box');
+//Creating array from nodelist of "box"
+let boxArray = Array.from('box');
+
+let clearButton = document.getElementById('clear');
+
+//Now that I have an array, I can cycle through this array using the forEach method
+//For each element in the array of box, perform the following funciton:
+box.forEach(function(target) {
+
+    //whatever the target element is, I'm going to add an event listener
+    //This event listener will display the text of each box 
+    target.addEventListener('click', function() {
+
+        target.innerHTML = target.dataset.text;
+
+    })
+
+})
+
